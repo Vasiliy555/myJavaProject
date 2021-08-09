02a80ef43c5309b4871bba073348ff98765fb87a
Сергей Сенчук <71141443+Vasiliy555@users.noreply.github.com>
2021-08-09 14:22:07 +0300

    Update Park.java

--- javalessons/src/main/java/school/lesson4/task7/Park.java
+++ javalessons/src/main/java/school/lesson4/task7/Park.java
@@ -1,43 +1,40 @@
 package school.lesson4.task7;
 
 public class Park {
-    class Train {
-        private String workingHours = "Работает: с 9 до 20 ежедневно";
-        private String name = "Поезд";
-        private int cost = 2;
-        public void info() {
-            System.out.printf("Аттракцион - %s %s. Стоимость: %d", name, workingHours,cost);
+    private String workingHours = "Работает: с 9 до 20 ежедневно";
+    private String name;
+    private int cost = 2;
+
+
+    class Att {
+
+        public void infoTrain() {
+            name = "Поезд";
+            System.out.printf("Аттракцион - %s. %s . Стоимость: %d",name, workingHours,cost);
             System.out.println();
         }
-        public void cost() {
-            System.out.printf("Аттракцион - %s. Стоимость: %d", name, cost);
+        public void infoWheel() {
+           name = "Колесо";
+            System.out.printf("Аттракцион - %s. %s . Стоимость: %d", name, workingHours,cost);
             System.out.println();
         }
-        public void open() {
-            System.out.printf("Аттракцион - %s. %s", name, workingHours);
+        public void infoVeterok() {
+            name = "Ветерок";
+            System.out.printf("Аттракцион - %s. %s . Стоимость: %d",name, workingHours,cost);
             System.out.println();
         }
-        public void name() {
-            System.out.printf("Аттракцион - %s", name);
-            System.out.println();
-        }
-    }
-    void infoTrain () {
-        Train chu= new Train();
-        System.out.printf("Аттракцион - %s. %s. Стоимость: %d", chu.name, chu.workingHours,chu.cost);
-        System.out.println();
     }
 
 
 
 
     public static void main(String[] args) {
-        Park.Train train = new Park().new Train();
-        train.name();
-        train.cost();
-        train.open();
+        Park.Att info = new Park().new Att();
 
-        Park park1 = new Park();
-        park1.infoTrain();
+
+info.infoTrain();
+info.infoWheel();
+info.infoVeterok();
+
     }
 }
